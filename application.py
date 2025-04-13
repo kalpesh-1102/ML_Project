8f4ef3b090423b868289a1710562e045414e2147
kalpesh-1102 <kalpeshpathade03531@gmail.com>
2025-04-13 21:50:30 +0530

    Deployment Configuration

--- application.py
+++ application.py
@@ -33,4 +33,4 @@ def predict_datapoint():
         return render_template('home.html', results=round(results[0], 2))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000, debug=True)
+    app.run(host="0.0.0.0",port=5000)
